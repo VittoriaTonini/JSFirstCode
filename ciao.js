92816f96a38180a7e0eafcf115a511abb143e039
vittoriatonini <vitto.tonini@gmail.com>
2025-11-11 19:09:32 +0100

    First commit of the project

---
+++ ciao.js
@@ -0,0 +1,4 @@
+// Node.js è un ambiente di esecuzione JS fuori dal browser
+// Ci permette di eseguire script JS direttamente dal terminale.
+
+console.log("Ciao! Questo è il mio primo script in Node.js");
